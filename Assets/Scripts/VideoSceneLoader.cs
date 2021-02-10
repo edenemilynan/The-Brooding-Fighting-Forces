54d814716f056cd71d10f42e3615396cba17b99a
JandaileD <43227764+JandaileD@users.noreply.github.com>
2021-02-10 12:49:04 -0700

    Janny's stuff
    includes scenes, scripts, backgrounds, videos, sprites, and prefabs

---
+++ Assets/Scripts/VideoSceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoSceneLoader : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public void Start()
+    {
+        videoPlayer.loopPointReached += LoadNextScene;
+
+    }
+
+
+    public void LoadNextScene(VideoPlayer vp)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+}
