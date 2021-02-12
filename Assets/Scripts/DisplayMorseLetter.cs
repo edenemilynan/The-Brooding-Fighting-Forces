b0125aabfcd4ddbf941faf55d67218fe3ed8d1ad
Eden <eknechte@ualberta.ca>
2021-02-12 16:11:35 -0700

    Trying display morse

---
+++ Assets/Scripts/DisplayMorseLetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayMorseLetter : MonoBehaviour
+{
+    public TextMeshProUGUI morseText;
+    private string morseLetter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject morseInput = GameObject.Find("MorseInputs");
+        SpaceBarInput input = (SpaceBarInput)morseInput.GetComponent<SpaceBarInput>();
+        morseLetter = input.GetCurrentMorseLetter();
+
+        if (morseLetter != null)
+        {
+            morseText.text = morseLetter;
+        }
+    }
+}
