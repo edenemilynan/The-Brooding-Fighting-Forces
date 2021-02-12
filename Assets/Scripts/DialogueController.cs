d4a50e13a6d262061449a2e47a7e0d560057c5fc
Eden <eknechte@ualberta.ca>
2021-02-12 15:08:21 -0700

    Wonk dialogue

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    private bool firstConversation = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject go = GameObject.Find("somegameobjectname");
+        //ScriptB other = (ScriptB)go.GetComponent(typeof(ScriptB));
+        //other.DoSomething()
+
+        //FindObjectOfType<DialogueTrigger>().TriggerDialogue();
+        
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstConversation == false)
+    {
+        GameObject conversation = GameObject.Find("FirstConversation");
+        DialogueTrigger other = (DialogueTrigger)conversation.GetComponent<DialogueTrigger>();
+        firstConversation = true;
+
+        if (other != null)
+        {
+            other.TriggerDialogue();
+        }
+    }
+        
+    }
+}
