57421b0862aeaec4ce2af870eebb34b7c271f6df
cwyz_dev <chris@wwwoods.xyz>
2021-03-15 23:26:46 -0600

    New audios
    Added a twinkly wink SFX to the Bexos moon, and modified the wink timings to make it not annoying
    
    Added TitleTrack and Chapter1_BGM audio files, so that we don't need placeholder music anymore

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,30 +10,29 @@ public class MainMenu : MonoBehaviour
 
     private SpriteRenderer spriteRend;
 
+    public AudioSource ad;
     void Start()
     {
         spriteRend = GetComponent<SpriteRenderer>();
         spriteRend = NotWinking;
         spriteRend.sortingOrder = 1;
-
     }
     void Update()
     {
         timePassed += Time.deltaTime;
         //Debug.Log(timePassed);
         // Every few seconds, switch between images
-        if(timePassed >= 2)
+        if(timePassed >= 15 && spriteRend.sortingOrder == 1)
         {
             timePassed = 0;
-            if(spriteRend.sortingOrder == 1)
-            {
-                spriteRend.sortingOrder = -1;
-            }
-            else
-            {
-                spriteRend.sortingOrder = 1;
-            }
+            spriteRend.sortingOrder = -1;
+            ad.Play();
 
+        }
+        else if (timePassed >= 1 && spriteRend.sortingOrder == -1)
+        {
+             timePassed = 0;
+             spriteRend.sortingOrder = 1;
         }
 
     }
