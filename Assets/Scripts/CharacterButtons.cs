2a5baab2ab571f2e2211dcb0c5ae650fe08d3483
JandaileD <43227764+JandaileD@users.noreply.github.com>
2021-03-22 19:58:16 -0600

    Character selection changes
    - Button now don't work again once selected
    - When finishing a chapter, goes back to the character selection screen

---
+++ Assets/Scripts/CharacterButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterButtons : MonoBehaviour
+{
+    public Button petuniaB, swethB, mentoB;
+    public static bool petuniaNotClicked = true, swethNotClicked = true, mentoNotClicked = true;
+
+    //Sets the buttons interability when the scene is loaded again
+    void Start()
+    {
+        petuniaB.interactable = petuniaNotClicked;
+        swethB.interactable = swethNotClicked;
+        mentoB.interactable = mentoNotClicked;
+    }
+
+}
