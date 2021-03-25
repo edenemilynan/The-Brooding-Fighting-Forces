1e112970c461c4abc86667e922c18ffa8ac214a8
JandaileD <43227764+JandaileD@users.noreply.github.com>
2021-03-24 21:48:02 -0600

    Implementing sorting
    - adding the start to sorting

---
+++ Assets/Scripts/SortingManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingManager : MonoBehaviour
+{
+    public SpriteRenderer shape1, shape2, shape3, valid;
+    public SortingCombinations sc;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rand;
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            rand = Random.Range(0, sc.combinations.Count);
+            shape1.sprite = sc.shapes[(int)sc.combinations[rand][0]];
+            shape2.sprite = sc.shapes[(int)sc.combinations[rand][1]];
+            shape3.sprite = sc.shapes[(int)sc.combinations[rand][2]];
+            
+            if(sc.combinations[rand][3] == SortingCombinations.Shape.valid)
+            {
+                valid.sprite = sc.shapes[6];
+            }
+            else
+            {
+                valid.sprite = sc.shapes[3];
+            }
+            Debug.Log(sc.combinations[rand][3]);
+        }
+    }
+}
