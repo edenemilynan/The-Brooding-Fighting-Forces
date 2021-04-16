ecee56e4ce407d9f3eeb36d628334e6e04094b45
Eden <eknechte@ualberta.ca>
2021-04-15 18:32:11 -0600

    Boxes for chapter 3 animated

---
+++ Assets/Scripts/DesktopControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopControls : MonoBehaviour
+{
+
+    public Animator callFromBexos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void bexosCalling()
+    {
+        callFromBexos.SetBool("visible", true);
+    }
+
+    public void hideBexosCall()
+    {
+        callFromBexos.SetBool("visible", false);
+    }
+
+}
