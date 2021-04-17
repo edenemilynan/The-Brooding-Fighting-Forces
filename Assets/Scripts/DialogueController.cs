130660d81b64fa5f888ce3f908ed9caa6a8202cf
JandaileD <43227764+JandaileD@users.noreply.github.com>
2021-04-17 02:50:08 -0600

    Sounds and stuff
    - Added sound effects to chapter 3

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -346,6 +346,7 @@ public class DialogueController : MonoBehaviour
             }
             TriggerThisDialogue(dialogueName);
             triggerManager.Ch2ConfirmCleanSystemConversation = TriggerManager.convoStatus.Complete;
+            SceneManager.LoadScene(1);
         }
 
         
